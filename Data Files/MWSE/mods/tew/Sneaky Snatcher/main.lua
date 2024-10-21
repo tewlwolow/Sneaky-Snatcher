e6fbbcd57998f5d16edf30d6d60246f01e30f1e6
tewlwolow <leonczernecki@gmail.com>
2024-10-21 23:08:11 +0200

    Cleanup

--- Data Files/MWSE/mods/tew/Sneaky Snatcher/main.lua
+++ Data Files/MWSE/mods/tew/Sneaky Snatcher/main.lua
@@ -1,37 +1,45 @@
 local config = require("tew.Sneaky Snatcher.config")
 
+-- Flag to control whether player has been detected by NPCs
 ---@type boolean
 local playerDetected
 
+-- Timestamp for last detection attempt
 ---@type number
 local lastChecked = 0
 
+-- Flag for whether the container has been accessed
 ---@type boolean
 local accessed = true
 
+-- A table to track references (yes, again and again)
 ---@type tes3reference[]
 local refs = {}
 
+-- Do our thing in the activate event
 --- @param e activateEventData
 local function activateCallback(e)
+    local actTarget = e.target
     if (e.activator == tes3.player) and
         (not playerDetected) and
         (tes3.getSimulationTimestamp(false) - (lastChecked) < 2) and
         (tes3.mobilePlayer.isSneaking) and
-        (e.target.object.objectType == tes3.objectType.container and not e.target.lockNode) and
-        not tes3.hasOwnershipAccess { target = e.target } and
-        ((not e.target.tempData.sneakySnatcher) or
-            (e.target.tempData.sneakySnatcher and not e.target.tempData.sneakySnatcher.accessed)) then
-        tes3.messageBox { message = "First time accessed, progressing sneak..." }
+        (actTarget.object.objectType == tes3.objectType.container and not actTarget.lockNode) and not
+        (tes3.hasOwnershipAccess { target = actTarget }) and
+        (
+            (not actTarget.tempData.sneakySnatcher) or
+            (actTarget.tempData.sneakySnatcher and not actTarget.tempData.sneakySnatcher.accessed)
+        ) then
         accessed = true
-        e.target.tempData.sneakySnatcher = {}
-        e.target.tempData.sneakySnatcher.accessed = accessed
-        table.insert(refs, e.target)
+        actTarget.tempData.sneakySnatcher = {}
+        actTarget.tempData.sneakySnatcher.accessed = accessed
+        table.insert(refs, actTarget)
         tes3.mobilePlayer:exerciseSkill(tes3.skill.sneak, config.sneakSkillIncrease)
     end
 end
 event.register(tes3.event.activate, activateCallback)
 
+-- Track detection attempts
 --- @param e detectSneakEventData
 local function detectSneakCallback(e)
     if (e.target == tes3.mobilePlayer) and (e.detector.object.objectType == tes3.objectType.npc) then
@@ -41,11 +49,7 @@ local function detectSneakCallback(e)
 end
 event.register(tes3.event.detectSneak, detectSneakCallback)
 
--- Registers MCM menu --
-event.register(tes3.event.modConfigReady, function()
-    dofile("Data Files\\MWSE\\mods\\tew\\Sneaky Snatcher\\mcm.lua")
-end)
-
+-- Clear flags and tracker on cell change
 --- @param e cellChangedEventData
 local function cellChangedCallback(e)
     for _, ref in ipairs(refs) do
@@ -55,8 +59,14 @@ local function cellChangedCallback(e)
 end
 event.register(tes3.event.cellChanged, cellChangedCallback)
 
+-- Reset timestamp on game loaded
 --- @param e loadedEventData
 local function loadedCallback(e)
     lastChecked = 0
 end
 event.register(tes3.event.loaded, loadedCallback)
+
+-- Registers MCM menu --
+event.register(tes3.event.modConfigReady, function()
+    dofile("Data Files\\MWSE\\mods\\tew\\Sneaky Snatcher\\mcm.lua")
+end)
